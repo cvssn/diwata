d0e7ec00505be0a333fa8fad39830ba2193985f3
cavassani <lcs.cvssn.qrz@gmail.com>
2026-03-13 01:36:44 -0300

    compila os directives `@apply`

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const path = require('node:path');
+
+module.exports = {
+    plugins: {
+        tailwindcss: {
+            config: path.join(__dirname, 'config/tailwind.config.js')
+        },
+        
+        autoprefixer: {}
+    }
+}
